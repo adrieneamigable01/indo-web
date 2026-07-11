e25cb8318ba46c56c52b94f1a8a602097ced4232
Adriene Amigable <adrienecarrreamigable01@gmail.com>
2026-07-11 15:04:34 +0800

    updated

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_ENV', 'development'); // change to production on live
+define('APP_ENV', 'production'); // change to production on live
 
 define('APP_BASE', APP_ENV === 'development'
     ? '/indo-lending-system/'
