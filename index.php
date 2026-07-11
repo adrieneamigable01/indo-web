93f60c1e6b2ca87a532c8053ae8ba09c2af41d5f
Adriene Amigable <adrienecarrreamigable01@gmail.com>
2026-07-11 15:11:44 +0800

    updated

--- index.php
+++ index.php
@@ -4,10 +4,18 @@ require_once __DIR__ . '/config.php';
 
 $currentRoute = trim($_GET['route'] ?? '', '/');
 
+// Change this to your project folder if needed.
+// Examples:
+// '/'                  -> http://localhost/
+// '/lending/'          -> http://localhost/lending/
+// '/LendingPro/'       -> http://localhost/LendingPro/
+
+
 $base = APP_BASE;
 
 function url($path)
 {
+
     global $base;
     return rtrim($base, '/') . '/' . ltrim($path, '/');
 }
@@ -18,24 +26,6 @@ function isActive($route)
 
     return $currentRoute === $route ||
            str_starts_with($currentRoute, $route . '/');
-}
-
-$route = trim($_GET['route'] ?? '', '/');
-
-$routes = require 'routes.php';
-
-if (isset($routes[$route])) {
-    require $routes[$route];
-} else {
-    http_response_code(404);
-
-    if (file_exists(__DIR__ . '/pages/error/404.php')) {
-        require __DIR__ . '/pages/error/404.php';
-    } else {
-        echo "<h1>404 - Page Not Found</h1>";
-    }
-
-    exit;
 }
 
 
